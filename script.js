91bd820fc2b97282693fb4f0e366f0cc03eacd47
Aakash <kanduaakash43@gmail.com>
2025-03-12 17:50:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Typing effect for hero section
   var typed = new Typed("#typed-text", {
-    strings: ["Developer", "Cybersecurity Enthusiast", "Stock Bot Creator"],
+    strings: ["Ethical Hacker", "Developer", "Bot Developer", "Cybersecurity Developer", "AI Developer", "Web Developer", "Full-Stack Developer", "Python Developer", "App Developer", "Trading Bot Developer", "Telegram Bot Developer", "FinTech Developer", "UI/UX Developer", "Automation Developer", "Software Developer".
+
+             ],
     typeSpeed: 50,
     backSpeed: 30,
     loop: true,
